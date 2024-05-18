076b49fd6147fada6b8e2ad293cc9c079b778529
mn-48 <nazmul.cse48@gmail.com>
2024-05-19 00:10:12 +0600

    TodoItem component

---
+++ src/components/TodoItems.tsx
@@ -0,0 +1,5 @@
+const TodoItems: React.FC<{ text: string }> = (props) => {
+  return <li>{props.text}</li>;
+};
+
+export default TodoItems;
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Todo from '../models/todo';
-
+import Todo from "../models/todo";
+import TodoItems from './TodoItems';
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <TodoItems key={item.id} text={item.text}/>
       ))}
     </ul>
   );
