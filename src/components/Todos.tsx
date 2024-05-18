d0ddce059f05e3f3ecf8933d91ad85dfe86cf9de
mn-48 <nazmul.cse48@gmail.com>
2024-05-18 23:21:14 +0600

    initial code

---
+++ src/components/Todos.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Todos = () => {
+    return (
+        <ul>
+            <li>react todos</li>
+            <li>typescripts todos</li>
+            
+        </ul>
+    );
+};
+
+export default Todos;
