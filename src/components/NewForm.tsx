89fd122e6292e6c8df3ce11e78fb7438a5e9593e
mn-48 <nazmul.cse48@gmail.com>
2024-05-19 00:48:01 +0600

    useRef hook with ts

--- src/components/NewForm.tsx
+++ src/components/NewForm.tsx
@@ -1,11 +1,21 @@
+import { useRef } from "react";
+
 const NewForm = () => {
+  const todoTextInput = useRef<HTMLInputElement>(null);
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+    const enteredText = todoTextInput.current!.value;
+    if (enteredText.trim().length ===0){
+        // throw an error
+        return;
+    }
+
+
   };
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="text">Todo Text</label>
-      <input type="text" id="text" />
+      <input type="text" id="text" ref={todoTextInput} />
       <button>Add Todo</button>
     </form>
   );
